cc3e48ddca32d0e5c79fb461699eb532cccebc87
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-05-23 20:40:13 +0300

    add an exception to ArrayGetterSetter

--- src/ArrayGetterSetter.php
+++ src/ArrayGetterSetter.php
@@ -2,47 +2,66 @@
 
 namespace Dios\System\Multicasting;
 
+use Dios\System\Multicasting\Interfaces\MulticastingEntity;
+use Dios\System\Multicasting\Interfaces\ArrayEntity;
+
 /**
  * Getter and setter for entities with arrays.
  */
 trait ArrayGetterSetter
 {
     /**
-     * Gets values from the instance or the attribute ($propertyOfEntityValues).
+     * Gets values from the instance or the attribute ($propertyForEntity).
      *
      * @return array
      */
     public function getValuesFromInstance(): array
     {
-        /** @var MulticastingEntity|null $instance **/
+        /** @var MulticastingEntity|ArrayEntity|null $instance **/
         $instance = $this->getInstance();
 
         if ($instance) {
+            $this->throwExceptionWhenInvalidInterface($instance);
             $values = $instance->toArray();
-        } elseif (isset($this->{$this->propertyOfEntityValues}) && is_array($this->{$this->propertyOfEntityValues})) {
-            $values = $this->{$this->propertyOfEntityValues};
+        } elseif (isset($this->{$this->propertyForEntity}) && is_array($this->{$this->propertyForEntity})) {
+            $values = $this->{$this->propertyForEntity};
         }
 
         return $values ?? [];
     }
 
     /**
-     * Sets values to the instance and the attribute ($propertyOfEntityValues).
+     * Sets values to the instance and the attribute ($propertyForEntity).
      *
      * @param array $values
      */
     public function setValuesToInstance(array $values)
     {
-        /** @var MulticastingEntity|null $instance **/
+        /** @var MulticastingEntity|ArrayEntity|null $instance **/
         $instance = $this->getInstance();
 
         // Fills and gets data to update the current value of the property
         // with normalized data.
         if ($instance) {
+            $this->throwExceptionWhenInvalidInterface($instance);
             $instance->fillFromArray($values);
             $values = $instance->toArray();
         }
 
-        $this->{$this->propertyOfEntityValues} = $values;
+        $this->{$this->propertyForEntity} = $values;
+    }
+
+    /**
+     * Throws the exception when it is invalid interface.
+     *
+     * @param MulticastingEntity $instance
+     *
+     * @throws Exception
+     */
+    public function throwExceptionWhenInvalidInterface(MulticastingEntity $instance): void
+    {
+        if (! ($instance instanceof ArrayEntity)) {
+            throw new \Exception('The given instance is invalid interface. The instance must be implements ' . ArrayEntity::class);
+        }
     }
 }
