3662b21761c30c62c2cb7c84d043dfd72d16d3b5
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-06-15 21:25:02 +0300

    add TODO for tests

---
+++ tests/AttributeMulticastingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+
+class AttributeMulticastingTest extends TestCase
+{
+    // TODO
+    // 1. getTypes
+    // 2. getTypeByKey
+    // 3. getKeys and types
+    // 4. get
+    // 5. Test cache
+    // 5. default entity handler
+    // 6. Test each function.
+    // 7. Test exceptions
+    // 8. Test working of models and instances
+    // Use empty model, full model, model with default properties
+}
